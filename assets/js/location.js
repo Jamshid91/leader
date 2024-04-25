c6c4005e9ee9b158fc38d7299e0dfdc3e5c26dd0
Jamshid91 <iymon0217@gmail.com>
2024-04-25 16:57:08 +0500

    added

---
+++ assets/js/location.js
@@ -0,0 +1,24 @@
+const locIcons = document.querySelectorAll('.locIcon');
+const loc = document.querySelector('.location');
+const locName = document.querySelector('.location .name');
+const locAdd = document.querySelector('.location .address-text');
+
+locIcons.forEach(e => {
+    e.addEventListener('mousemove', function() {
+        window.onmousemove = function(event) {
+            x=event.clientX
+            y=event.clientY
+
+            loc.style.left = x-20+'px'
+            loc.style.top = y-20+'px'
+        }
+        locName.textContent = e.dataset.name;
+            locAdd.textContent = e.dataset.address;
+
+            loc.style.opacity = 1
+    });
+
+    e.addEventListener('mouseout', function() {
+        loc.style.opacity = 0
+    })
+})
