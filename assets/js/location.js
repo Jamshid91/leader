0d605d9458dd6dc186f59abaeb7e591fd170bda9
Jamshid91 <iymon0217@gmail.com>
2024-04-27 09:25:38 +0500

    added

--- assets/js/location.js
+++ assets/js/location.js
@@ -3,22 +3,36 @@ const loc = document.querySelector('.location');
 const locName = document.querySelector('.location .name');
 const locAdd = document.querySelector('.location .address-text');
 
-locIcons.forEach(e => {
-    e.addEventListener('mousemove', function() {
-        window.onmousemove = function(event) {
-            x=event.clientX
-            y=event.clientY
 
-            loc.style.left = x-20+'px'
-            loc.style.top = y-20+'px'
-        }
-        locName.textContent = e.dataset.name;
-            locAdd.textContent = e.dataset.address;
 
-            loc.style.opacity = 1
-    });
-
-    e.addEventListener('mouseout', function() {
-        loc.style.opacity = 0
+  if(window.matchMedia('(min-width: 769px)').matches) {
+    locIcons.forEach(e => {
+        e.addEventListener('mousemove', function() {
+            window.onmousemove = function(event) {
+                x=event.clientX
+                y=event.clientY
+    
+                loc.style.left = x-30+'px'
+                loc.style.top = y-30+'px'
+            }
+            locName.textContent = e.dataset.name;
+                locAdd.textContent = e.dataset.address;
+    
+                loc.style.opacity = 1
+        });
+    
+        e.addEventListener('mouseout', function() {
+            loc.style.opacity = 0
+        })
     })
-})
+  }
+
+  if(window.matchMedia('(max-width: 768px)').matches) {
+    locIcons.forEach(e => {
+        e.addEventListener('click', function() {
+            locName.textContent = e.dataset.name;
+            locAdd.textContent = e.dataset.address;
+            loc.style.display = 'flex'
+        });
+    })
+  }
